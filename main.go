0da3315793a24c45793a7a3fa7211973de7a87ed
gleydsonnn <30751736+gleydsonnn@users.noreply.github.com>
2022-02-03 15:41:43 -0300

    Update main.go

--- main.go
+++ main.go
@@ -6,18 +6,11 @@ import (
 	"log"
 )
 
-const webContent = "Olá mundo! Vamos crescer Sila e Lage kkkkkk
-
-
-(`_´)
-<,︻╦╤─  - -
-/﹋\"
-
 func main() {
 	http.HandleFunc("/", helloHandler)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, webContent)
+	fmt.Fprint("ola mundo")
 }
